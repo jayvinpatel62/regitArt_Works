5bfe45c0f1713ccd7b92d7506be0f0a9bb33c13e
Jayvin Patel <60234153+jayvinpatel62@users.noreply.github.com>
2024-10-30 11:21:48 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 
+$(window).on('load', function() { // makes sure the whole site is loaded 
+  $('#status').fadeOut(); // will first fade out the loading animation 
+  $('#preloader').delay(500).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(500).css({'overflow':'visible'});
+})
+
 $('.my-table-model').hide();
   
 $('.tr-click-model').click(function(){
